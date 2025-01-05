ce67b4a58e00a4a3bb76a00aab33667d8643dc1d
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-01-05 20:37:45 +0900

    ✅ Test

--- intergration/intergation.ts
+++ intergration/intergation.ts
@@ -4,9 +4,15 @@ import { Bridge } from '../types';
 export interface AppBridgeState extends Bridge {
   token: string;
   setToken(token: string): Promise<void>;
+  count: number;
+  increase: () => Promise<void>;
 }
 
 export const appBridge = bridge<AppBridgeState>(({ get, set }) => ({
+  count: 0,
+  async increase() {
+    set({ count: get().count + 1 });
+  },
   token: '',
   async setToken(token: string) {
     set({ token });
